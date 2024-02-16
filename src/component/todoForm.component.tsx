f403b20c0788a4ebef7bfcf61285a3c753cf1845
Renz Halagao <rhalagao@village88.com>
2024-02-16 10:50:37 +0800

    Handle error and loading state of adding todo.

--- src/component/todoForm.component.tsx
+++ src/component/todoForm.component.tsx
@@ -6,10 +6,10 @@ import axios from 'axios';
 const TodoForm = () => {
   const ref = useRef<HTMLInputElement>(null);
   const queryClient = useQueryClient(); 
-  const addTodo = useMutation({
+  const addTodo = useMutation<Todo, Error, Todo>({
     mutationFn: (todo: Todo) => 
         axios
-            .post("https://jsonplaceholder.typicode.com/todos", todo)
+            .post("https://jsonplaceholder.typicode.com/todoas", todo)
             .then(res => res.data),
     onSuccess: (saved_todo) => {
         queryClient.setQueryData<Todo[]>(["todos"], todos => [saved_todo, ...(todos || [])] )
@@ -17,27 +17,36 @@ const TodoForm = () => {
   });
 
   return (
-    <form 
-        className="row mb-3 mt-2 ms-1" 
-        onSubmit={(event)=>{
-            event.preventDefault();
-            if(ref.current && ref.current.value){
-                addTodo.mutate({
-                    id: 0,
-                    title: ref.current.value,
-                    completed: false,
-                    userId: 1,
-                })
-            }
-        }}
-    >
-      <div className="col">
-        <input ref={ref} type="text" className="form-control" />
-      </div>
-      <div className="col">
-        <button className="btn btn-primary">Add</button>
-      </div>
-    </form>
+    <>
+      <form 
+          className="row mb-3 mt-2 ms-1" 
+          onSubmit={(event)=> {
+              event.preventDefault();
+              if(ref.current && ref.current.value){
+                  addTodo.mutate({
+                      id: 0,
+                      title: ref.current.value,
+                      completed: false,
+                      userId: 1,
+                  })
+              }
+          }}
+      >
+        <div className="col">
+          <input ref={ref} type="text" className="form-control" />
+        </div>
+        <div className="col">
+          <button disabled={addTodo.isLoading} className="btn btn-primary">{(addTodo.isLoading) ? "Adding..." : "Add"}</button>
+        </div>
+      </form>
+
+      {addTodo.error &&
+        <div className="alert alert-danger ms-2 p-2" role="alert">
+            {addTodo.error.message}
+        </div>
+      }
+
+    </>
   );
 };
 
